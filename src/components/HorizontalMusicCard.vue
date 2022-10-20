51514a2ac9563495a243d036daabd27aadd2240a
Eyetu Kingsley <eyetukingsley330@gmail.com>
2022-10-20 12:04:35 +0100

    I have done the track and volume sliders and addead the heart icon to the horiontal cards.

--- src/components/HorizontalMusicCard.vue
+++ src/components/HorizontalMusicCard.vue
@@ -1,10 +1,16 @@
 <template>
-    <div class="bg-brand-black rounded-[20px] px-[18px] py-[18px] flex gap-[14px]">
-        <img class="rounded-[10px] w-[70px] h-[70px]" src="@/assets/images/music-1.svg"/>
-        <div class="leading-none">
-            <a class="block text-base font-semibold leading-none mb-[5px]">Golden age of 80s</a>
-            <a class="block text-sm text-white font-medium text-opacity-50 mb-2">Sean Swadder</a>
-            <p class="block text-sm font-medium">2:34:45</p>
+    <div class="bg-brand-black rounded-[20px] pl-[18px] pr-[22px] py-[18px] flex justify-between items-center">
+        <div class="flex gap-[14px]">
+            <img class="rounded-[10px] w-[70px] h-[70px]" src="@/assets/images/music-1.svg"/>
+            <div class="leading-none">
+                <a class="block text-base font-semibold leading-none mb-[5px]">Golden age of 80s</a>
+                <a class="block text-sm text-white font-medium text-opacity-50 mb-2">Sean Swadder</a>
+                <p class="block text-sm font-medium">2:34:45</p>
+            </div>
         </div>
+
+        <span class="p-2 border border-white border-opacity-10 rounded-full w-10 h-10 flex items-center justify-center">
+            <svg width="21" height="21" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M2.15388 8.69874C1.34913 6.18624 2.28963 3.31449 4.92738 2.46474C6.31488 2.01699 7.84638 2.28099 8.99988 3.14874C10.0911 2.30499 11.6789 2.01999 13.0649 2.46474C15.7026 3.31449 16.6491 6.18624 15.8451 8.69874C14.5926 12.6812 8.99988 15.7487 8.99988 15.7487C8.99988 15.7487 3.44838 12.7277 2.15388 8.69874Z" stroke="#FACD66" stroke-width="0.5625" stroke-linecap="round" stroke-linejoin="round"/><path opacity="0.4" d="M12 5.02501C12.8025 5.28451 13.3695 6.00076 13.4377 6.84151" stroke="#FACD66" stroke-width="0.5625" stroke-linecap="round" stroke-linejoin="round"/></svg>
+        </span>
     </div>
 </template>
