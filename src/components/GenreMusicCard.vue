ce68c92b8acf008961efe576bee113e32a181af0
Eyetu Kingsley <eyetukingsley330@gmail.com>
2022-10-23 21:53:24 +0100

    Modified the cards a bit.

--- src/components/GenreMusicCard.vue
+++ src/components/GenreMusicCard.vue
@@ -15,7 +15,7 @@
             <p class="font-semibold text-[#d5d5d5] w-1/5 mr-6">4:17</p>
             <p class="font-semibold text-[#d5d5d5] w-[45%] mr-6">September 19th, 2022</p>
             <div class="w-[10%]">
-                <vs-dropdown :vs-trigger-click="true">
+                <vs-dropdown>
                     <vs-button type="success-gradient">
                         <svg class="cursor-pointer" width="25" height="25" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.25782 8.94595C8.63788 8.94595 8.94598 8.63785 8.94598 8.25779C8.94598 7.87774 8.63788 7.56964 8.25782 7.56964C7.87777 7.56964 7.56967 7.87774 7.56967 8.25779C7.56967 8.63785 7.87777 8.94595 8.25782 8.94595Z" stroke="#FACD66" stroke-width="1.3763" stroke-linecap="round" stroke-linejoin="round"/><path d="M8.25782 4.12887C8.63788 4.12887 8.94598 3.82077 8.94598 3.44072C8.94598 3.06066 8.63788 2.75256 8.25782 2.75256C7.87777 2.75256 7.56967 3.06066 7.56967 3.44072C7.56967 3.82077 7.87777 4.12887 8.25782 4.12887Z" stroke="#FACD66" stroke-width="1.3763" stroke-linecap="round" stroke-linejoin="round"/><path d="M8.25782 13.763C8.63788 13.763 8.94598 13.4549 8.94598 13.0749C8.94598 12.6948 8.63788 12.3867 8.25782 12.3867C7.87777 12.3867 7.56967 12.6948 7.56967 13.0749C7.56967 13.4549 7.87777 13.763 8.25782 13.763Z" stroke="#FACD66" stroke-width="1.3763" stroke-linecap="round" stroke-linejoin="round"/></svg>
                     </vs-button>
@@ -27,7 +27,8 @@
                         <vs-dropdown-item>
                             Remove
                         </vs-dropdown-item>
-                        <vs-dropdown-item divider>
+                        <divider class="my-1 block h-[0.5px] bg-white bg-opacity-40" />
+                        <vs-dropdown-item>
                             Add to Playlist
                         </vs-dropdown-item>
                         <vs-dropdown-item>
@@ -66,7 +67,7 @@
 
 <style>
     .vs-dropdown--menu--after {
-        background-color: #4e5052 !important;
+        background-color: #393d3f !important;
     }
 
     .vs-dropdown-menu.notHeight .vs-dropdown--menu--after {
